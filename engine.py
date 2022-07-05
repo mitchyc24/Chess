df2a6435e7e5f5eec856693047277ddf63297a08
Mitchell Carroll <mitchell.carroll24@gmail.com>
2022-07-05 14:11:40 -0400

    created method to generate FEN

--- engine.py
+++ engine.py
@@ -22,12 +22,14 @@ class Chess_Engine:
     '''
     def __init__(self) -> None:
         self.move_log = []
-        self.current_turn = 0 #0 for white, 1 for black
+        self.current_turn = "w"
+        self.castling = "KQkq"
+        self.en_passant = "-"
+        self.halfmove_clock = 0
+        self.fullmoves = 1
         self.current_board = np.array([
         [Rook("black"), Knight("black"), Bishop("black"), Queen("black"), King("black"), Bishop("black"), Knight("black"), Rook("black")],
         [Pawn("black"), Pawn("black"), Pawn("black"), Pawn("black"), Pawn("black"), Pawn("black"), Pawn("black"), Pawn("black")],
-        [None,None,None,None,None,None,None,None],
-        [None,None,None,None,None,None,None,None],
         [None,None,None,None,None,None,None,None],
         [None,None,None,None,None,None,None,None],
         [None,None,None,None,None,None,None,None],
@@ -40,4 +42,24 @@ class Chess_Engine:
     
 
     def get_current_FEN(self):
-        return "r2qk3/p1pp1p1p/5n2/1B6/3bbQ2/2P2R2/PP5P/RNB3rK w q - 1 16"
+        piece_str = ""
+        for i, row in enumerate(self.current_board):
+            space_count = 0
+            for j, square in enumerate(row):
+                if square:
+                    if space_count:
+                        piece_str += str(space_count)
+                        space_count = 0
+                    piece_str += square.FEN_letter
+                else:
+                    if j==7:
+                        piece_str += str(space_count)
+                    else:
+                        space_count += 1
+
+            if i != 7:    
+                piece_str += "/"
+
+        fen = " ".join([piece_str, self.current_turn, self.castling, str(self.en_passant), str(self.halfmove_clock), str(self.fullmoves)])  
+        print(fen) 
+        return fen
